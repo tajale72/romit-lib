9949ef1845f714330eb2901ca8110b1ef1881255
romit tajale <romittajale@unknown1e1f780a08e4.attlocal.net>
2025-06-04 13:59:48 -0500

    feat add more logger function

--- logger/logger.go
+++ logger/logger.go
@@ -6,15 +6,19 @@ import (
 )
 
 type RomitLogger struct {
-	infoLogger *log.Logger
-	warnLogger *log.Logger
+	infoLogger  *log.Logger
+	warnLogger  *log.Logger
+	errorLogger *log.Logger
+	debugLogger *log.Logger
+	traceLogger *log.Logger
 }
 
 func NewRomitLogger() *RomitLogger {
 	flags := log.Ldate | log.Ltime | log.Lshortfile
 	return &RomitLogger{
-		infoLogger: log.New(os.Stdout, "INFO: ", flags),
-		warnLogger: log.New(os.Stdout, "WARN: ", flags),
+		infoLogger:  log.New(os.Stdout, "INFO: ", flags),
+		warnLogger:  log.New(os.Stdout, "WARN: ", flags),
+		errorLogger: log.New(os.Stdout, "ERROR: ", flags),
 	}
 }
 
@@ -24,4 +28,24 @@ func (l *RomitLogger) LogInfo(message string) {
 
 func (l *RomitLogger) LogWarning(message string) {
 	l.warnLogger.Output(2, message)
+}
+
+func (l *RomitLogger) LogError(message string) {
+	l.errorLogger.Output(2, message)
+}
+
+func (l *RomitLogger) LogFatal(message string) {
+	l.errorLogger.Fatal(message)
+}
+
+func (l *RomitLogger) LogPanic(message string) {
+	l.errorLogger.Panic(message)
+}
+
+func (l *RomitLogger) LogDebug(message string) {
+	l.debugLogger.Output(2, message)
+}
+
+func (l *RomitLogger) LogTrace(message string) {
+	l.traceLogger.Output(2, message)
 }
